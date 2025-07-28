e91d906c2cd1433139a852bae098cc151b82fbf2
aghaSaqlain <03i08459082@gmail.com>
2025-07-28 18:23:20 +0500

    app.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero";
+
+import './App.css'; 
+function App() {
+  return (
+    <div className="app">
+      <Hero />
+      {/* <Projects />
+      <Experience />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
