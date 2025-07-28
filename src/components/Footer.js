e328d87cb58187f1ee8680365a1d00b5c5e79b77
aghaSaqlain <03i08459082@gmail.com>
2025-07-28 18:16:58 +0500

    removed unnecessary files

--- src/components/Footer.js
+++
@@ -1,6 +0,0 @@
-const Footer = () => (
-  <footer className="footer">
-    <p>&copy; {new Date().getFullYear()} Syed Muhammad Saqlain. All rights reserved.</p>
-  </footer>
-);
-export default Footer;
